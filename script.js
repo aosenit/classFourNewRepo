6e6b609ebce4738da06b399849b45d913bdfd783
oseni ade <oseniadedoyin22@gmail.com>
2022-01-08 15:50:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var menu = document.getElementById("menu");
+var headerNav = document.getElementById("headerNav");
+
+menu.addEventListener("click", function () {
+  headerNav.classList.toggle("showNav");
+});
