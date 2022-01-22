e0534754c1490d846e1ec5b97af7343a4c3faf4b
oseni ade <oseniadedoyin22@gmail.com>
2022-01-22 15:48:27 +0100

    mobile version completed

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ var headerNav = document.getElementById("headerNav");
 menu.addEventListener("click", function () {
   headerNav.classList.toggle("showNav");
 });
+
+headerNav.addEventListener("click", function () {
+  headerNav.classList.remove("showNav");
+});
